7bb6de8d6631abd57cfbbc848362f388ac1c187d
chensheng312 <chenyan1226@hnu.edu.cn>
2016-02-26 21:00:41 +0800

    Added files via upload
    ex7.6.h

---
+++ CP5_ex7_06_h.h
@@ -0,0 +1,34 @@
+#ifndef CP5_ex7_06_h
+#define CP5_ex7_06_h
+
+#include <string>
+struct Sale_data
+{
+    std::string isbn () const {return bookNo;}
+    Sale_data& combine(const Sale_data&);
+    double avg_price() const;
+
+    std::string bookNo;
+    unsigned units_sold=0;
+    double revenue = 0.0;
+};
+
+Sale_data add(const Sale_data&, const Sale_data&);
+std::ostream &print(std::ostream&, const Sale_data&);
+std::istream &read(std::istream&,Sale_data&);
+
+Sale_data& Sale_data::combine(const Sale_data& rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue +=rhs.revenue;
+    return *this;
+}
+double Sale_data::avg_price()const
+{
+    if(units_sold)
+        return revenue/units_sold;
+    else
+        return 0;
+}
+
+#endif // CP5_ex7_06_h
