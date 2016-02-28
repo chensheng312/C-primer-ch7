f88780eedfd06bbb054019bc0dc00a6a20d4f003
chensheng312 <chenyan1226@hnu.edu.cn>
2016-02-28 20:45:08 +0800

    Added files via upload

---
+++ 7_21.h
@@ -0,0 +1,73 @@
+#ifndef CP5_ex7_21_h
+#define CP5_ex7_21_h
+#include <string>
+#include<iostream>
+
+struct Sale_data;
+std::istream &read(std::istream& ,Sale_data& );
+
+class Sale_data
+{
+    friend Sale_data add(const Sale_data&, const Sale_data&);
+    friend std::istream &read(std::istream& ,Sale_data&);
+    friend std::ostream &print(std::ostream &os,const Sale_data &item);
+
+    public:
+    Sale_data() = default;
+    Sale_data(const std::string &s): bookNo(s) {}
+    Sale_data(const std::string &s, unsigned n, double p):
+        bookNo(s), units_sold(n), revenue(p*n) {}
+    Sale_data(std::istream &is)
+    {
+        read(is,*this);
+    }
+    //end
+    std::string isbn () const {return bookNo;}
+    Sale_data& combine(const Sale_data&);
+
+    private:
+    double avg_price() const;
+
+    std::string bookNo;
+    unsigned units_sold=0;
+    double revenue = 0.0;
+};
+
+
+Sale_data& Sale_data::combine(const Sale_data& rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue +=rhs.revenue;
+    return *this;
+}
+double Sale_data::avg_price()const
+{
+    if(units_sold)
+        return revenue/units_sold;
+    else
+        return 0;
+}
+
+
+
+std::istream &read(std::istream& is,Sale_data&item)
+{
+    double price = 0.0;
+    is>>item.bookNo>>item.units_sold>>price;
+    item.revenue = price * item.units_sold;
+    return is;
+
+}
+std::ostream &print(std::ostream &os,const Sale_data &item)
+{
+    os<<item.isbn()<<" "<<item.units_sold<<" "<<item.revenue
+    <<" "<<item.avg_price();
+    return os;
+}
+Sale_data add(const Sale_data &lhs,const Sale_data &rhs)
+{
+    Sale_data sum=lhs;
+    sum.combine(rhs);
+    return sum;
+}
+#endif // CP5_ex7_21_h
